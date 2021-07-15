b6d608d3d033c04a10f049bdb425aa32b65a1782
alambrow <alexander.lambrow@gmail.com>
2021-07-15 11:34:51 -0500

    ch 10 complete

--- attractions.py
+++ attractions.py
@@ -1,38 +1,35 @@
-class PettingZoo:
-    def __init__(self, name):
+class Attraction:
+
+    def __init__(self, name, description):
         self.attraction_name = name
-        self.description = "cute, cuddly critters and crepes"
+        self.description = description
         self.animals = list()
-    
+
     def add_animal(self, animal):
         self.animals.append(animal)
+
+    def remove_animal(self, animal):
+        self.animals.remove(animal)
+
+    def __str__(self):
+        return f'{self.attraction_name} ({len(self.animals)} animals)'
+
+    def __len__(self):
+        return len(self.animals)
     
     @property
     def last_critter_added(self):
         return self.animals[-1]
 
-class SnakePit:
-    def __init__(self, name):
-        self.attraction_name = name
-        self.description = "don't get bitten"
-        self.animals = list()
+class PettingZoo(Attraction):
+    def __init__(self, name, description="cute, cuddly critters and crepes"):
+        super().__init__(name, description)
     
-    def add_animal(self, animal):
-        self.animals.append(animal)
-    
-    @property
-    def last_critter_added(self):
-        return self.animals[-1]
 
-class Wetlands:
-    def __init__(self, name):
-        self.attraction_name = name
-        self.description = "wetlands and wet bar"
-        self.animals = list()
-    
-    def add_animal(self, animal):
-        self.animals.append(animal)
-    
-    @property
-    def last_critter_added(self):
-        return self.animals[-1]
+class SnakePit(Attraction):
+    def __init__(self, name, description="don't get bitten"):
+        super().__init__(name, description)
+
+class Wetlands(Attraction):
+    def __init__(self, name, description="wetlands and wet bar"):
+        super().__init__(name, description)
